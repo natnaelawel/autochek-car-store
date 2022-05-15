f3d203612f394d6d86b0d6588dc6d0c28d2f52a4
natnaelawel <natnael.awel@gmail.com>
2022-05-16 01:42:35 +0300

    first commit

--- imgs.ts
+++
@@ -1,4 +0,0 @@
-// https://demo.w3layouts.com/demos_new/template_demo/28-08-2018/electro_store-demo_Free/1204782700/web/images/b1.jpg
-// https://demo.w3layouts.com/demos_new/template_demo/28-08-2018/electro_store-demo_Free/1204782700/web/images/b2.jpg
-// https://demo.w3layouts.com/demos_new/template_demo/28-08-2018/electro_store-demo_Free/1204782700/web/images/b3.jpg
-// https://demo.w3layouts.com/demos_new/template_demo/28-08-2018/electro_store-demo_Free/1204782700/web/images/b4.jpg
